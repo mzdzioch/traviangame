71022f0433bad2a28abb76dbde6cee0385ae66eb
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-03-17 15:59:28 +0100

    GameWorld finished

--- src/main/java/com/sda/krk/controller/GameWorldController.java
+++ src/main/java/com/sda/krk/controller/GameWorldController.java
@@ -3,6 +3,7 @@ package com.sda.krk.controller;
 import com.sda.krk.model.GameWorld;
 import com.sda.krk.model.dto.GameWorldDto;
 import com.sda.krk.model.response.ResponseMessage;
+import com.sda.krk.model.response.StatusResponse;
 import com.sda.krk.services.IGameWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,11 @@ public class GameWorldController {
     @RequestMapping(path = "/create", method = RequestMethod.POST)
     public ResponseMessage<GameWorld> createGameWorld(@RequestBody GameWorldDto gameWorldDto) {
         Optional<GameWorld> gameWorld = gameWorldService.createGameWorldService(gameWorldDto);
-        return null;
+
+        if (gameWorld.isPresent()) {
+            return new ResponseMessage<>(StatusResponse.OK, "world created", gameWorld.get());
+        } else {
+            return new ResponseMessage<>(StatusResponse.REQUEST_ERROR, "unable to create world", null);
+        }
     }
 }
--- src/main/java/com/sda/krk/services/GameWorldService.java
+++ src/main/java/com/sda/krk/services/GameWorldService.java
@@ -1,12 +1,18 @@
 package com.sda.krk.services;
 
+import com.sda.krk.dao.GameWorldFieldRepository;
 import com.sda.krk.dao.GameWorldRepository;
 import com.sda.krk.model.GameWorld;
+import com.sda.krk.model.GameWorldField;
 import com.sda.krk.model.dto.GameWorldDto;
 import com.sda.krk.model.factory.GameWorldFactory;
+import com.sda.krk.model.factory.GameWorldFieldFactory;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +21,21 @@ public class GameWorldService implements IGameWorldService {
     @Autowired
     private GameWorldRepository gameWorldRepository;
 
+    @Autowired
+    private GameWorldFieldRepository gameWorldFieldRepository;
+
     @Override
     public Optional<GameWorld> createGameWorldService(GameWorldDto gameWorldDto) {
-        GameWorld gameWorld = gameWorldRepository.save(GameWorldFactory.createGameWorld(gameWorldDto));
+        GameWorld gameWorld = null;
+        try {
+            gameWorld = gameWorldRepository.save(GameWorldFactory.createGameWorld(gameWorldDto));
+            if (gameWorld != null) {
+                List<GameWorldField> fields = GameWorldFieldFactory.createFieldsForWorld(gameWorld);
+                gameWorldFieldRepository.save(fields);
+            }
+        } catch (DataIntegrityViolationException exception) {
+            Logger.getLogger(getClass()).error("Duplicated entry name.", exception);
+        }
         return Optional.ofNullable(gameWorld);
     }
 }
